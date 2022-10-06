d64499ba48eeb42ff79691fbb1eb11ad6b577a4c
yosymfony <yosymfony@gmail.com>
2022-10-06 23:05:13 +0200

    Added workspace dir to GIT safe direcories list

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -l
 export CO_REPOSITORY_KEY="$1"
+echo git --version
+echo "Adding directory $GITHUB_WORKSPACE to GIT's safe directories list"
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 /client/entrypoint.sh
